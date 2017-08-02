ef98b0c388bb6df16764095e69227ad6723851e6
qiaoshi <2642656099@qq.com>
2017-08-02 15:21:02 +0800

    feature-1

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.administrator.yy;
 
-
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-private ViewPager pager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toast.makeText();
+        feature();
+    }
+
+    private void feature() {
+
+        Toast.makeText(MainActivity.this,"这是第一个分支",Toast.LENGTH_LONG).show();
     }
 }
