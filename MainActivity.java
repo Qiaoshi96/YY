0c2e02fb5ca6496a2ca98639e42e534fe07ac4ca
qiaoshi <2642656099@qq.com>
2017-08-02 15:04:22 +0800

    第一次啊

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.yy;
+
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+private ViewPager pager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toast.makeText();
+    }
+}
